a2f0c667b10621bacd0878bf91b9228f8efa3b74
Arturo Miranda <mirandaarturo45@gmail.com>
2023-04-21 20:53:10 -0300

    guia4 ejercicio1 terminado

--- Guia4/src/Ejercicios/Ejercicio1.java
+++ Guia4/src/Ejercicios/Ejercicio1.java
@@ -4,17 +4,70 @@
  */
 package Ejercicios;
 
+import java.util.Scanner;
+
 /**
  *
  * @author artur
  */
 public class Ejercicio1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer numero: ");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Ingrese el segundo numero: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Ingrese la operacion que desea realizar:");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicacion");
+        System.out.println("4. Division");
+        int opcion = sc.nextInt();
+
+        double resultado = 0;
+
+        switch (opcion) {
+            case 1:
+                resultado = suma(num1, num2);
+                break;
+            case 2:
+                resultado = resta(num1, num2);
+                break;
+            case 3:
+                resultado = multiplicacion(num1, num2);
+                break;
+            case 4:
+                resultado = division(num1, num2);
+                break;
+            default:
+                System.out.println("Opcion invalida.");
+                break;
+        }
+
+        System.out.println("EL resultado es: " + resultado);
     }
-    
+
+    public static double suma(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double resta(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiplicacion(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double division(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("No se puede dividir por cero.");
+            return 0;
+        }
+        return num1 / num2;
+    }
 }
