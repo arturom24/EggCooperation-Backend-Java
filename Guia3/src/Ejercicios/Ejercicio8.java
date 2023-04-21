cee44ef1ad417ab8602a1416e4209324aa62549e
Arturo Miranda <mirandaarturo45@gmail.com>
2023-04-21 00:08:25 -0300

    guia3 ejercicio 8 terminado

---
+++ Guia3/src/Ejercicios/Ejercicio8.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author artur
+ */
+public class Ejercicio8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa el tamaño del cuadrado: ");
+        int n = sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0 || i == n -1 || j == 0 || j == n -1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
