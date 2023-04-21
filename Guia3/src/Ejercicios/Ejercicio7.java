840d46e1d1e72dba79d3b01531956092f0de731d
Arturo Miranda <mirandaarturo45@gmail.com>
2023-04-20 23:58:34 -0300

    guia3 ejercicio 7 terminado

---
+++ Guia3/src/Ejercicios/Ejercicio7.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author artur
+ */
+public class Ejercicio7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int correctCount = 0;
+        int incorrectCount = 0;
+        while (true) {
+            System.out.print("Ingrese una cadena (debe tener 5 caracteres, comenzar con X y terminar con O): ");
+            String input = sc.nextLine();
+            if (input.equals("&&&&&")) {
+                break;
+            }
+            if (input.length() != 5) {
+                System.out.println("La cadena debe tener exactamente 5 caracteres.");
+                incorrectCount++;
+            } else if (input.charAt(0) != 'X') {
+                System.out.println("La cadena debe comenzar con X.");
+                incorrectCount++;
+            } else if (input.charAt(4) != 'O') {
+                System.out.println("La cadena debe terminar con O.");
+                incorrectCount++;
+            } else {
+                System.out.println("Cadena válida.");
+                correctCount++;
+            }
+        }
+        System.out.println("Numero de lecturas correctas: " + correctCount);
+        System.out.println("Numero de lecturas incorrectas: " + incorrectCount);
+    }
+
+}
