ff7f6f576eb545853482a70c176155f41c53d94a
Arturo Miranda <mirandaarturo45@gmail.com>
2023-05-03 20:57:56 -0300

    guia5 ejercicio1 terminado

--- Guia5/src/Ejercicios/Ejercicio1.java
+++ Guia5/src/Ejercicios/Ejercicio1.java
@@ -3,7 +3,26 @@ package Ejercicios;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[] numeros = new int[100];
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = i;
+        }
+
+        for (int i = 0; i < numeros.length - 1; i++) {
+            for (int j = i + 1; j < numeros.length; j++) {
+                if (numeros[i] < numeros[j]) {
+                    int temp = numeros[i];
+                    numeros[i] = numeros[j];
+                    numeros[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+
     }
 
 }
