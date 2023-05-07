89642bb3b1b7e109a4f3ef573a6ecc60eaf79430
Arturo Miranda <mirandaarturo45@gmail.com>
2023-05-07 17:30:02 -0300

    guia7 ejercicio2 terminado

--- Guia7/src/Ejercicios/Ejercicio2/Ejercicio2.java
+++ Guia7/src/Ejercicios/Ejercicio2/Ejercicio2.java
@@ -1,9 +1,17 @@
 package Ejercicios.Ejercicio2;
 
+import Ejercicios.Ejercicio2.Entidades.Circunferencia;
+
 public class Ejercicio2 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Circunferencia circ1 = new Circunferencia();
+        
+        circ1.crearCircunferencia();
+        System.out.println("Circunferencia 1:");
+        System.out.println("Radio: " + circ1.getRadio());
+        System.out.println("Area: " + circ1.area());
+        System.out.println("Perimetro: " + circ1.perimetro());
     }
 
 }
--- Guia7/src/Ejercicios/Ejercicio2/Entidades/Circunferencia.java
+++ Guia7/src/Ejercicios/Ejercicio2/Entidades/Circunferencia.java
@@ -1,5 +1,44 @@
 package Ejercicios.Ejercicio2.Entidades;
 
+import java.util.Scanner;
+
 public class Circunferencia {
+
+    private double radio;
+
+    public Circunferencia(float radio) {
+        this.radio = radio;
+    }
+
+    public Circunferencia() {
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+
+    public void crearCircunferencia() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el valor del radio:");
+        double radio = sc.nextDouble();
+        this.radio = radio;
+    }
+
+    public double area() {
+        return Math.PI * radio * radio;
+    }
+
+    public double perimetro() {
+        return 2 * Math.PI * radio;
+    }
+
+    @Override
+    public String toString() {
+        return "Circunferencia{" + "radio=" + radio + '}';
+    }
 
 }
