39ef4de689a7a91f1ee8c14ddd19d78bd50bd3f3
Arturo Miranda <mirandaarturo45@gmail.com>
2023-05-10 22:35:30 -0300

    guia8 ejercicio1 completado

--- Guia8/src/Ejercicios/Ejercicio1/Ejercicio1.java
+++ Guia8/src/Ejercicios/Ejercicio1/Ejercicio1.java
@@ -1,9 +1,22 @@
 package Ejercicios.Ejercicio1;
 
+import Ejercicios.Ejercicio1.Entidades.CuentaBancaria;
+import Ejercicios.Ejercicio1.Servicios.CuentaBancariaServicio;
+
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        CuentaBancariaServicio servicio = new CuentaBancariaServicio();
+
+        CuentaBancaria cuenta1 = servicio.crearCuenta();
+
+        servicio.ingresar(cuenta1, 100);
+        servicio.consultarSaldo(cuenta1);
+        servicio.extraccionRapida(cuenta1);
+        servicio.consultarSaldo(cuenta1);
+        servicio.extraccionRapida(cuenta1);
+        servicio.consultarSaldo(cuenta1);
+        servicio.retirar(cuenta1, 200);
     }
 
 }
