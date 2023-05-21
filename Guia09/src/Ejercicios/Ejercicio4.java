9246ef44682eea3b031d2f6f085b16db8aa7b5ac
Arturo Miranda <mirandaarturo45@gmail.com>
2023-05-21 13:44:44 -0300

    guia9 ejercicio4 terminado

--- Guia09/src/Ejercicios/Ejercicio4.java
+++ Guia09/src/Ejercicios/Ejercicio4.java
@@ -1,9 +1,20 @@
 package Ejercicios;
 
+import Ejercicios.Servicios.FechaServicio;
+import java.util.Date;
+
 public class Ejercicio4 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Date fechaNacimiento = FechaServicio.fechaNacimiento();
+        System.out.println("Fecha de nacimiento: " + fechaNacimiento);
+        
+        Date fechaActual = FechaServicio.fechaActual();
+        System.out.println("Fecha actual: " + fechaActual);
+        
+        int diferenciaAnios = FechaServicio.diferencia(fechaNacimiento, fechaActual);
+        System.out.println("Edad del usuario: " + diferenciaAnios + " años.");
     }
 
 }
