43d2b8e48bb4b8205c31fcc6ef3a39d73946eea9
Arturo Miranda <mirandaarturo45@gmail.com>
2023-05-21 18:09:50 -0300

    guia9 todos los ejercicios terminados

---
+++ Guia09/src/Ejercicios/Ejercicio6.java
@@ -0,0 +1,15 @@
+package Ejercicios;
+
+import Ejercicios.Entidades.Curso;
+
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+
+        curso.crearCurso();
+
+        curso.calcularGananciaSemanal();
+    }
+
+}
