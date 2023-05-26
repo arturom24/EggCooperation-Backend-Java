06795f4452c104102cdf241d41ef81d843f451d2
Arturo Miranda <mirandaarturo45@gmail.com>
2023-05-25 23:16:59 -0300

    guia10 carga inicial clase main

---
+++ Guia10/src/Ejercicios/Ejercicio4.java
@@ -0,0 +1,9 @@
+package Ejercicios;
+
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ Guia10/src/Ejercicios/Ejercicio5.java
@@ -0,0 +1,9 @@
+package Ejercicios;
+
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
