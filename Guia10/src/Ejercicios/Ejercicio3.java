25b3e76aa667acbac583b229f02f1dc11c45166f
Arturo Miranda <mirandaarturo45@gmail.com>
2023-05-29 20:35:18 -0300

    guia10 ejercicio3 terminado

--- Guia10/src/Ejercicios/Ejercicio3.java
+++ Guia10/src/Ejercicios/Ejercicio3.java
@@ -1,9 +1,26 @@
 package Ejercicios;
 
+import Ejercicios.Servicios.AlumnoService;
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        AlumnoService servicio = new AlumnoService();
+        servicio.crearAlumno();
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del alumno para calcular su nota:");
+        String nombreAlumno = sc.nextLine();
+        
+        double notaFinal = servicio.notaFinal(nombreAlumno);
+        if (notaFinal != -1) {
+            System.out.println("La nota final de " + nombreAlumno + " es: " + notaFinal);
+        } else {
+            System.out.println("El alumno no se encuentra en la lista.");
+        }
+        
+        servicio.mostrarAlumnos();
     }
 
 }
