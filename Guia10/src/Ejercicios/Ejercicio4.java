39604cf98e89fb94a5b9c673ccbf0602f6554b44
Arturo Miranda <mirandaarturo45@gmail.com>
2023-05-30 20:47:34 -0300

    guia10 ejercicio4 terminado

--- Guia10/src/Ejercicios/Ejercicio4.java
+++ Guia10/src/Ejercicios/Ejercicio4.java
@@ -1,9 +1,27 @@
 package Ejercicios;
 
+import Ejercicios.Servicios.PeliculaService;
+
 public class Ejercicio4 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        PeliculaService servicio = new PeliculaService();
+
+        servicio.crearPelicula();
+        
+        servicio.mostrarPeliculas();
+        
+        servicio.mostrarPeliculasDuracionMayorAUnaHora();
+        
+        servicio.ordenarPeliculasPorDuracionDescendente();
+        
+        servicio.ordenarPeliculasPorDuracionAscendente();
+        
+        servicio.ordenarPeliculasPorTitulo();
+        
+        servicio.ordenarPeliculasPorDirector();
     }
+    
+    
 
 }
--- Guia10/src/Ejercicios/Entidades/Pelicula.java
+++ Guia10/src/Ejercicios/Entidades/Pelicula.java
@@ -2,4 +2,25 @@ package Ejercicios.Entidades;
 
 public class Pelicula {
 
+    private final String titulo;
+    private final String director;
+    private final double duracion;
+
+    public Pelicula(String titulo, String director, double duracion) {
+        this.titulo = titulo;
+        this.director = director;
+        this.duracion = duracion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public double getDuracion() {
+        return duracion;
+    }
 }
