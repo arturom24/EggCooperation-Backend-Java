8028aea332e2805b4083c1a970bca47cbd71c673
Arturo Miranda <mirandaarturo45@gmail.com>
2023-05-31 23:39:37 -0300

    guia10 ejercicio5 entidades terminado

---
+++ Guia10/src/Ejercicios/Entidades/Pais.java
@@ -0,0 +1,41 @@
+package Ejercicios.Entidades;
+
+import java.util.Objects;
+
+public class Pais {
+
+    private final String nombre;
+
+    public Pais(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Verificar si los objetos son el mismo
+        if (this == obj) {
+            return true;
+        }
+
+        // Verificar si el objeto pasado como argumento es nulo o pertenece a una clase diferente
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        // Realizar casting del objeto a la clase Pais
+        Pais otroPais = (Pais) obj;
+
+        // Comparar los atributos nombre de los objetos utilizando Objects.equals()
+        return Objects.equals(nombre, otroPais.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        // Generar un código hash basado en el atributo nombre utilizando Objects.hash()
+        return Objects.hash(nombre);
+    }
+}
