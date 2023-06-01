564704a242e4855f13194382941b0e5c4cc471c3
Arturo Miranda <mirandaarturo45@gmail.com>
2023-06-01 00:03:08 -0300

    guia10 ejercicio5 terminado

--- Guia10/src/Ejercicios/Ejercicio5.java
+++ Guia10/src/Ejercicios/Ejercicio5.java
@@ -1,9 +1,19 @@
 package Ejercicios;
 
+import Ejercicios.Servicios.PaisService;
+
 public class Ejercicio5 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        PaisService servicio = new PaisService();
+        
+        servicio.agregarPais();
+        
+        servicio.mostrarPaises();
+        
+        servicio.mostrarPaisesOrdenados();
+        
+        servicio.eliminarPais();
     }
 
 }
